95311287fb851f94f3b5cfcaa44055252b32ef33
Gretar Atli Gretarsson <gretar@precisdigital.com>
2022-12-06 13:56:01 +0100

    keybinding changed for copilot

--- lua/plugin_configs/copilot.lua
+++ lua/plugin_configs/copilot.lua
@@ -1,10 +1,10 @@
 -- Sources
 -- https://www.reddit.com/r/neovim/comments/sk70rk/using_github_copilot_in_neovim_tab_map_has_been/
--- https://www.google.com/search?q=copilot+neovim&source=lnms&tbm=vid&sa=X&ved=2ahUKEwiCnoWT4N_7AhVORfEDHQwsB9oQ_AUoAnoECAEQBA&biw=2749&bih=1578&dpr=1.05#fpstate=ive&vld=cid:ffe6f9a5,vid:eMnZBaOs4vM
+-- https://www.google.com/search?q=copiloit+neovim&source=lnms&tbm=vid&sa=X&ved=2ahUKEwiCnoWT4N_7AhVORfEDHQwsB9oQ_AUoAnoECAEQBA&biw=2749&bih=1578&dpr=1.05#fpstate=ive&vld=cid:ffe6f9a5,vid:eMnZBaOs4vM
 -- https://github.com/LunarVim/LunarVim/issues/1856#issuecomment-954224770
 
 vim.g.copilot_no_tab_map = true
-vim.cmd[[imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")]]
+vim.cmd[[imap <silent><script><expr> <C-f> copilot#Accept("\<CR>")]] -- <C-D> stands for control delete
 
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
