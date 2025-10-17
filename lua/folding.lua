7dae6eaa3ce57e8da67ff8fa46f112b87ac52e73
Gretar Atli Gretarsson <gretar@precisdigital.com>
2025-10-17 14:48:59 +0200

    sss

--- lua/folding.lua
+++ lua/folding.lua
@@ -37,17 +37,16 @@ require('ufo').setup({
 })
 
 -- Refresh folds on insert/leave. This forces neovim to update the folds when exit insert mode.
-vim.api.nvim_create_autocmd(
-  { "TextChanged", "TextChangedI", "InsertLeave", "BufWinEnter" },
-  {
-    callback = function()
-      if vim.bo.buftype == "" then
-        vim.cmd("silent! normal! zX")
-      end
-    end,
-  }
-)
-
+-- vim.api.nvim_create_autocmd(
+--   { "TextChanged", "TextChangedI", "InsertLeave", "BufWinEnter" },
+--   {
+--     callback = function()
+--       if vim.bo.buftype == "" then
+--         vim.cmd("silent! normal! zX")
+--       end
+--     end,
+--   }
+-- )
 
 -- Remember folds per file
 vim.opt.viewoptions:append("folds")
